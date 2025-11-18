6a1c6ac90e192edb77d89d53f578458757b895f9
Ekaterina Novikova <fearnocait@gmail.com>
2025-11-18 17:03:15 +0300

    [fix] кнопки в User

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -2,6 +2,8 @@ import type { FC } from "react";
 import { Button } from "../components/button/button";
 
 import styles from "./pages.module.css";
+import { logout } from "../utils/utils";
+import { useNavigate } from "react-router-dom";
 
 type UserProps = {
   name: string;
@@ -9,14 +11,24 @@ type UserProps = {
 
 export const User: FC<UserProps> = (props) => {
   const {name} = props;
+  const navigate = useNavigate();
+  const onCLickLogout = () => {
+    logout();
+    navigate('/');
+  }
+
+  const onClickContacts = () => {
+    navigate('/contacts');
+  }
+
   return (
     <>
       <h1 className={styles.headline}>
         Привет, {name}
       </h1>
       <div className={styles.buttons_container}>
-        <Button>Выйти из аккаунта</Button>
-        <Button>Перейти в контакты</Button>
+        <Button onClick={onCLickLogout}>Выйти из аккаунта</Button>
+        <Button onClick={onClickContacts}>Перейти в контакты</Button>
       </div>
     </>
   );
