d19fd353cbbe212c499997f34764f3e6a3307307
Ekaterina Novikova <fearnocait@gmail.com>
2025-11-18 17:47:42 +0300

    [fix] disabled кнопка

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,24 @@
-import type { FC, ReactElement } from 'react';
-import styles from './button.module.css';
+import type { FC, ReactElement } from "react";
+import styles from "./button.module.css";
 
 type ButtonProps = {
   children: string | ReactElement;
   onClick?: () => void;
-  type?: 'button' | 'submit';
+  type?: "button" | "submit";
   disabled?: boolean;
-}
+};
 
 export const Button: FC<ButtonProps> = (props) => {
-  const {children, onClick, type, disabled} = props;
-  return <button onClick={onClick} className={styles.button} type={type} disabled={disabled}>{children}</button>;
-}
+  const { children, onClick, type, disabled } = props;
+  const buttonClass = `${styles.button} ${disabled ? styles.button_disabled : styles.button_enabled}`
+  return (
+    <button
+      onClick={onClick}
+      className={buttonClass}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
