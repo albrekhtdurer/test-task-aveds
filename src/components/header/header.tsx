7efef1e7203ec658a9b03e9c467ddf188f342b61
Ekaterina Novikova <fearnocait@gmail.com>
2025-11-18 14:49:40 +0300

    [fix] роутинг

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.svg'
-import styles from './header.module.css';
-import type { FC } from 'react';
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import styles from "./header.module.css";
+import type { FC } from "react";
 
 export const Header: FC = () => {
+  const mainLocation = {
+    pathname: "/",
+  };
   return (
     <header className={styles.header}>
-      <Link to='/'><img alt='логотип' src={logo}></img></Link>
+      <Link to="/">
+        <img alt="логотип" src={logo}></img>
+      </Link>
       <div className={styles.header__link_container}>
-      <Link to='/contacts' className={styles.header__link}>Контакты</Link>
-      <Link to='/login' className={styles.header__link}>Войти</Link>
+        <Link to="/contacts" className={styles.header__link}>
+          Контакты
+        </Link>
+        <Link to="/login" state={{background: mainLocation}} className={styles.header__link}>
+          Войти
+        </Link>
       </div>
     </header>
   );
-}
+};
